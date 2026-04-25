f9c9a773c989dc856bf6e572de4443ff459d53cc
nasr <kar.nasr@yahoo.com>
2026-04-25 13:05:21 +0200

    deuxieme commit

---
+++ projet c++ basique/projet/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  projet
+//
+//  Created by nasr on 25/04/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    //std::cout << "Hello, World!\n";
+    //std::cout << "Hello, World!\n";
+    std::cout << "Bonjour, j'écris du C++" << std::endl <<
+    "Et j'aime beaucoup.";
+    
+    std::cout << 'S' << 'a' << 'l' << 'u' << 't' << ' ' << 't' <<
+    'o' << 'i' << ' ' << '!' << std::endl;
+    
+    return EXIT_SUCCESS;
+}
