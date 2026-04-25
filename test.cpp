bc5dc413fc172d71bac1c41bc8766aa4b078a54a
nasr <kar.nasr@yahoo.com>
2026-04-25 12:30:31 +0200

    premier comit

---
+++ test.cpp
@@ -0,0 +1,20 @@
+//
+//  test.cpp
+//  
+//
+//  Created by nasr on 12/04/2026.
+//
+
+#include "test.hpp"
+
+int main()
+{
+std::cout << "Hello World !" << std::endl; // affiche Hello
+    std::cout << "Bonjour, j'écris du C++" << std::endl <<
+"Et j'aime beaucoup.";
+    std::cout << '\n' << 'S' << 'a' << 'l' << 'u' << 't' << ' ' << 't' <<
+'o' << 'i' << ' ' << '!' << std::endl;
+    std::cout << "Il m'a demandé \" Comment vas-tu ? \"" << std::endl;
+return 0;
+}
+
---
+++ test.hpp
@@ -0,0 +1,13 @@
+//
+//  test.hpp
+//  
+//
+//  Created by nasr on 12/04/2026.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
