9b4e0631c7d499c954e39c28c397837f6574ae8b
nasr <kar.nasr@yahoo.com>
2026-04-26 09:55:16 +0200

    ajout exemple vector etalgorithmr

--- projet c++ basique/projet/main.cpp
+++ projet c++ basique/projet/main.cpp
@@ -1,21 +1,51 @@
-//
-//  main.cpp
-//  projet
-//
-//  Created by nasr on 25/04/2026.
-//
+
+
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    //std::cout << "Hello, World!\n";
-    //std::cout << "Hello, World!\n";
-    std::cout << "Bonjour, j'écris du C++" << std::endl <<
-    "Et j'aime beaucoup.";
-    
-    std::cout << 'S' << 'a' << 'l' << 'u' << 't' << ' ' << 't' <<
-    'o' << 'i' << ' ' << '!' << std::endl;
-    
-    return EXIT_SUCCESS;
+template <typename T>
+void afficher(const T& c)
+{
+    for(auto e:c)
+        std::cout << e << "";
+    std::cout << std::endl;
 }
+
+
+int main()
+{
+    std:: vector<double> notes = { 3, 8, 5, 15, 17, 17, 9} ;
+    auto d= notes.begin(),f=notes.end();
+    afficher(notes);
+    
+    //copy
+    std:: vector<double> notes1;
+    std::copy(d, f, std::back_inserter(notes1));
+    afficher(notes1);
+    
+    //copy if
+    std:: vector<double> notes2;
+    std::copy_if(d, f, std::back_inserter(notes2), [](double n){return n>=10;});
+    std::cout << "notes >=10  ";
+    afficher(notes2);
+    
+    
+    // copy n
+    std::deque<double> notes3;
+    std::copy_n(d, 3, back_inserter(notes3)),std::cout <<"3 prem notes=";afficher(notes3);
+    
+    // copy backward
+    std::vector<double>notes4(notes1.size()+5);
+    std::copy_backward(d, f, notes4.end());
+    std::cout << "notes copiés à la fin=  ";
+    afficher(notes4);
+    
+    
+    
+    
+    return 0;
+}
+
+
+
+
